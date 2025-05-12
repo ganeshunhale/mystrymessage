be07a441d01e1998ff10c132b6c38c77cece3c83
ganeshunhale <ganeshunhale.15@gmail.com>
2025-05-12 17:21:55 +0530

    changes in signup css

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -77,7 +77,7 @@ const page =()=>{
   
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p8 space-y-8 bg-white rounded-lg">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg">
 
         <div className="text-center">
           <h1 className = "text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
@@ -102,7 +102,7 @@ const page =()=>{
                 This is your public display name.
               </FormDescription> */}
               {isCheckingUsername && <Loader2/>}
-              {usernameMessage}
+              <p className={`text-sm ${usernameMessage=="User name is available"?'text-green-500':'text-red-500'}`}>{usernameMessage}</p>
               <FormMessage />
             </FormItem>
           )}
